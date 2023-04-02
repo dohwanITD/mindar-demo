30a05f128941ffb1e8b6a84d13b81fd83cd6f5db
Dohawn Park <soseullua@gmail.com>
2023-04-02 12:13:15 +0900

    build

---
+++ dist/mindar-face.prod.js
@@ -0,0 +1,12 @@
+import { C as o } from "./controller-285ba9c8.js";
+import { U as r } from "./ui-610aec4e.js";
+const i = {
+  Controller: o,
+  UI: r
+};
+window.MINDAR || (window.MINDAR = {});
+window.MINDAR.FACE = i;
+export {
+  o as Controller,
+  r as UI
+};
---
+++ dist/mindar-image.prod.js
@@ -0,0 +1,13 @@
+import { C as o, a as r } from "./controller-ea210c53.js";
+import { U as i } from "./ui-610aec4e.js";
+window.MINDAR || (window.MINDAR = {});
+window.MINDAR.IMAGE = {
+  Controller: o,
+  Compiler: r,
+  UI: i
+};
+export {
+  r as Compiler,
+  o as Controller,
+  i as UI
+};
