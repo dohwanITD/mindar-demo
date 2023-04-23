7ecea90f8b4847110910a5bb18a10a4f982f1ce4
dohwanITD <111561560+dohwanITD@users.noreply.github.com>
2023-04-23 17:55:47 +0900

    Update

--- examples/aframe-only/aframe-ar/src/step-ctrl.js
+++ examples/aframe-only/aframe-ar/src/step-ctrl.js
@@ -1,20 +1,6 @@
 //
 AFRAME.registerComponent('step-ctrl', {
     schema: {
-        isLerping: { type: 'boolean', default: false },
-        positions: {
-            default: [
-                { x: 0.4, y: -0.4, z: 0.01 },
-                { x: 0.4, y: -0.4, z: 0 },
-                { x: 0.4, y: -0.4, z: 0 },
-                { x: 0.4, y: -0.4, z: 0 },
-                { x: 0.4, y: -0.4, z: 0 },
-                { x: 0.4, y: -0.4, z: 0 },
-                { x: 0.4, y: -0.4, z: 0 },
-                { x: 0.4, y: -0.4, z: 0 },
-            ],
-        },
-        currPosIdx: { type: 'number', default: 0 },
     },
     init() {
         console.log("Step-ctrl init");
@@ -30,7 +16,10 @@ AFRAME.registerComponent('step-ctrl', {
             let animInterval2 = null
 
             let isNeedRabbitIdle = false;
+            let isNeedOutherAnim = false;
             const MAX_STEP_LEN = 8;
+
+            const clapSound = 'clap'
 
             // const sceneEl = document.querySelector('a-scene');
             // const arSystem = sceneEl.systems["mindar-image-system"];
@@ -64,32 +53,37 @@ AFRAME.registerComponent('step-ctrl', {
 
             // TTS Script
             const captionList = [
-                // '「갓생살기 with 펀드」란? 특정한 목표를 정하고, 이를 성취하기 위해 생산적이고 계획적인 생활을 실천한다는의미의 MZ세대 유행어로, "펀드와 함께 재테크 목표달성을 위한 생활을 시작해보자"라는의미의 이벤트명 입니다.',
+                // '「갓생살기 with 펀드」란? 특정한 목표를 정하고, 이를 성취하기 위해 생산적이고 계획적인 생활을 실천한다는 의미의 MZ세대 유행어로, "펀드와 함께 재테크 목표달성을 위한 생활을 시작해보자"라는의미의 이벤트명 입니다.',
                 // '펀드신규 이벤트 기간은 5월 2일부터 6월 30일까지!!',
                 // '모든 상품이 대상은 아니고 참여운용사 상품을 가입했을때만 이벤트 대상이 된답니다!', 
                 // '임의식펀드 5백만원이상 또는 적립식펀드 30만원 이상, 신규및 자동이체를 등록한 고객중 1,623명을 추첨하여 다양한 경품을 드립니다~ ',
                 // '큰 금액을 가입한 고객에게는 더 큰 혜택을 드려요. 임의식펀드 5천만원이상 또는 적립식펀드 1백만원이상, 신규및 자동이체를 등록한 고객 중 510명을 추첨하여 경품을 드립니다~',  
                 // '연금저축펀드 TDF상품을 가입한고객중 100명을 추첨하여 베스킨라빈스 패밀리아이스크림 쿠폰도 드려요!',  
                 // '펀드 신규고객 총 2233명을 추첨하여 경품을 드리는 펀드신규이벤트입니다. 고객님께 이벤트를 안내하며 영업에 활용해 보세요! 기타 자세한 내용은 시행문을 참고해주세요.', 
-
-                '「갓생살기 with 펀드」',
-                '5월 2일부터 6월 30일까지!!',
+                
+                // 안녕하세요
+                '안녕하세요',
+                '「갓생살기 with 펀드」, 펀드와 함께 재테크 목표달성을 위한 생활을 시작해보자!',
+                '5월 2일부터 6월 30일까지',
                 '참여운용사 상품을 가입했을때만 이벤트 대상이 된답니다!', 
-                '1,623명을 추첨',
-                '510명을 추첨',  
-                '100명을 추첨하여 베스킨라빈스 패밀리아이스크림 쿠폰',  
-                '총 2233명', 
+                '임의식펀드 5백만원이상 또는 적립식펀드 30만원 이상 1,623명을 추첨',
+                ' 임의식펀드 5천만원이상 또는 적립식펀드 1백만원이상 510명을 추첨',  
+                '연금저축펀드 TDF상품을 가입한고객 중 100명을 추첨',  
+                '펀드 신규고객 총 2233명을 추첨 경품을 드리는 펀드신규이벤트!', 
+                '감사합니다.'
             ]
             const subtitle = document.getElementById('subtitleBtn')
-            subtitle.innerText = '「갓생살기 with 펀드」'
+            subtitle.innerText = '안녕하세요'
 
             // Audio - BGM & TTS ------
             let soundTTS = null // TTS
+            let soundEffect = null // TTS
 
             // BGM
             const soundMain = new window.Howl({
                 src: (`../../assets/audios/Cute_Avalanche_-_RKVC.mp3`),
                 loop: true,
+                autoplay: true,
                 volume: 0.2,
             })
             soundMain.play()
@@ -108,20 +102,32 @@ AFRAME.registerComponent('step-ctrl', {
 
             // Animation Evnet ----------
             rabbit.addEventListener('animation-finished', () => {
+
                 console.log('The Rabbit animation is finished!')
+
                 if (isNeedRabbitIdle) {
-                    rabbit.setAttribute('animation-mixer', { clip: 'Idle', loop: 'repeat', timeScale: 1 })
+                    isNeedRabbitIdle = false;
+                    console.log("isNeedRabbitIdle")
+                    rabbit.setAttribute('animation-mixer', { clip: 'Idle', loop: 'repeat', repetitions : 'Infinity' })
+                } else if (isNeedOutherAnim) {
+                    isNeedOutherAnim = false;
+                    isNeedRabbitIdle = true;
+                    console.log("isNeedOutherAnim")
+                    rabbit.setAttribute('animation-mixer', {
+                        clip: 'Delight',
+                        loop: 'repeat',
+                        repetitions : '2',
+                        timeScale: 1,
+                    })
                 }
-                // 최초 한 번만 실행, Run only once at first
             })
 
-            car.addEventListener('animation-finished', () => {
-                console.log('The car animation is finished!')
-                // 최초 한 번만 실행, Run only once at first
-            })
+            // car.addEventListener('animation-finished', () => {
+            //     console.log('The car animation is finished!')
+            // })
 
 
-             // 01: Idle      (Frame Start:  0,         Frame End :  48 )
+            // 01: Idle      (Frame Start:  0,         Frame End :  48 )
             // 02: Delight   (Frame Start: 60,         Frame End : 105 )
             // 03: Walk      (Frame Start: 115,        Frame End : 138 )
             // 04: Joy       (Frame Start: 149,        Frame End : 256 )
@@ -133,11 +139,12 @@ AFRAME.registerComponent('step-ctrl', {
             // 10: Gift_02 (Frame Start: 940 ,     Frame End :1180  )
             // 11: Gift_03 (Frame Start: 1252,     Frame End :1490 )
 
+            //Open_Door Close_Door Wheel_Rotate
+
             /// //////////////////////////////////////////////////////////
             /// -------------------- stepHandler -------------------- ///
             /// //////////////////////////////////////////////////////////
             const userClickHandler = () => {
-                console.log('Step Handler')
                 if (interval !== null) clearInterval(interval)
                 ++stepIdx
 
@@ -149,7 +156,7 @@ AFRAME.registerComponent('step-ctrl', {
                     soundTTS.stop()  // 플레이중인 음원이 있으면 스탑
                     soundTTS = null;
                 }
-                if (stepIdx > 0) {
+                if (stepIdx > 0 && stepIdx < 8) {
                     soundTTS = new window.Howl({
                         src: (`../../assets/audios/tts_0${stepIdx -1}.mp3`),
                     })
@@ -158,62 +165,66 @@ AFRAME.registerComponent('step-ctrl', {
                
                 // ---------- Animation ---------- walk spin point stand bow wave idle break
                 if (animInterval != null) {
-                    console.log("clearInterval")
                     clearInterval(animInterval)
                 }
-                const animationMixer = rabbit.getAttribute('animation-mixer')
-                if (animationMixer) {
-                    animationMixer.clampWhenFinished = true
-                    animationMixer.crossFadeDuration = 0.2
-                    // animationMixer.startAt = 0
-                }
+                
+                // const animationMixer = rabbit.getAttribute('animation-mixer')
+                // if (animationMixer) {
+                //     animationMixer.clampWhenFinished = true
+                //     animationMixer.crossFadeDuration = 0.2
+                //     animationMixer.startAt = 0
+                // }
               
-                if (stepIdx === 0) {
-                    rabbit.setAttribute('animation-mixer', { clip: 'Idle', loop: 'repeat' })
-                    // isNeedRabbitIdle = true;
+                isNeedRabbitIdle = false;
+                isNeedOutherAnim = false;
 
-                    //Open_Door Close_Door Wheel_Rotate
+                if (stepIdx === 0) { // 안녕하세요요
                     car.setAttribute('animation', {property: 'position', to: {x: 0, y: 0, z: 0}, dur: 4000, easing: 'easeInOutCubic', loop: false})
                     car.setAttribute('animation-mixer', { clip: 'Wheel_Rotate', loop: 'once' })
-
                     animInterval = setTimeout(() => {
-                        console.log("Open_Door");
-                        car.setAttribute('animation-mixer', { clip: 'Open_Door', loop: 'once' });
-                        rabbit.setAttribute('animation-mixer', { clip: 'Wave goodbye', loop: 'once' })
+                        car.setAttribute('animation-mixer', { clip: 'Open_Door', loop: 'once'});
+                        rabbit.setAttribute('animation-mixer', { clip: 'Wave_goodbye', loop: 'once' })
                         isNeedRabbitIdle = true;
                     }, 4000)
-                } else if (stepIdx === 1) {  // 「갓생살기 with 펀드」란?
-                    console.log(3);
-                    rabbit.setAttribute('animation-mixer', { clip: 'Joy', loop: 'repeat', repetitions : 3 })
+                } else if (stepIdx === 1) { // 「갓생살기 with 펀드」란?
+                    rabbit.setAttribute('animation-mixer', {clip: 'Joy', loop: 'repeat', repetitions: 3, crossFadeDuration: 0.4 })
                     isNeedRabbitIdle = true;
-
                 } else if (stepIdx === 2) { // 펀드신규 이벤트
-                    rabbit.setAttribute('animation-mixer', { clip: 'Pride', loop: 'once' })
+                    rabbit.setAttribute('animation-mixer', { clip: 'Pride', loop: 'repeat', repetitions : 2, crossFadeDuration: 0.4})
                     isNeedRabbitIdle = true;
 
                 } else if (stepIdx === 3) { // 모든 상품이 대상은 아니고
-                    rabbit.setAttribute('animation-mixer', { clip: 'Surprise', loop: 'once', timeScale: 0.5 })
-                    isNeedRabbitIdle = true;
+                    rabbit.setAttribute('animation-mixer', { clip: 'Surprise', loop: 'once', timeScale: 0.25, crossFadeDuration: 0.4 })
+                    isNeedOutherAnim = true;
 
                 } else if (stepIdx === 4) {
-                    rabbit.setAttribute('animation-mixer', { clip: 'Gift_01', loop: 'once' })
+                    rabbit.setAttribute('animation-mixer', { clip: 'Gift_01', loop: 'once', crossFadeDuration: 0.4 })
                     isNeedRabbitIdle = true;
 
                 } else if (stepIdx === 5) {
-                    rabbit.setAttribute('animation-mixer', { clip: 'Gift_02', loop: 'once' })
-                    isNeedRabbitIdle = true;
+                    rabbit.setAttribute('animation-mixer', { clip: 'Gift_02', loop: 'once', crossFadeDuration: 0.4 })
+                    isNeedOutherAnim = true;
 
                 } else if (stepIdx === 6) {
-                    rabbit.setAttribute('animation-mixer', { clip: 'Gift_03', loop: 'once' })
+                    rabbit.setAttribute('animation-mixer', { clip: 'Gift_03', loop: 'once', crossFadeDuration: 0.4 })
                     isNeedRabbitIdle = true;
 
                 } else if (stepIdx === 7) { // 펀드 신규고객 총 2233명
-                    rabbit.setAttribute('animation-mixer', { clip: 'Confetti', loop: 'once' })
-                    isNeedRabbitIdle = true;
-                 
-                } else if (stepIdx === 8) { // 펀드 신규고객 총 2233명
-                    rabbit.setAttribute('animation-mixer', { clip: 'Delight', loop: 'once' })
-                    isNeedRabbitIdle = true;
+                    rabbit.setAttribute('animation-mixer', { clip: 'Confetti', loop: 'repeat', repetitions : 4, crossFadeDuration: 0.4})
+                    isNeedOutherAnim = true;
+                   
+                    if (soundEffect !== null) {
+                        soundEffect.stop()  // 플레이중인 음원이 있으면 스탑
+                        soundEffect = null;
+                        soundEffect = new window.Howl({
+                            src: (`../../assets/audios/.mp3`),
+                        })
+                        soundEffect.play()
+                    }
+              
+                } else if (stepIdx === 8) { // 감사합니다
+                    rabbit.setAttribute('animation-mixer', { clip: 'Delight',  loop: 'repeat', repetitions : 2 , crossFadeDuration: 0.4})
+                    isNeedOutherAnim = true;
                 }
 
                 // setTimeout(() => {
